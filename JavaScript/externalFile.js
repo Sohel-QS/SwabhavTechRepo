4f7520a93f7d3739c905421ae28929b7595beb91
Sohel-Qs <manerisohel@gmail.com>
2021-11-26 17:02:41 +0530

    updated folder javascript

---
+++ JavaScript/externalFile.js
@@ -0,0 +1,8 @@
+document.writeln("<br><br>Code in Separate File<br>");
+function  ShowDistance(speed, time) {
+    document.writeln("<br>Distance :"+ speed*time);
+}
+
+ShowDistance(10, 5);
+ShowDistance(10, 10);
+ShowDistance(20, 4);
